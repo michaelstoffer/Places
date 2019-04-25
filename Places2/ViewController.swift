b36d715e813064ab4fed2fa3e909818f7b4db90b
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-04-24 22:26:37 -0400

    Initial Commit

---
+++ Places2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places2
+//
+//  Created by Michael Stoffer on 4/24/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
